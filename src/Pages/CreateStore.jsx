5193ef37f464ef2d28bb95536b7089ff895e7daa
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-09 23:17:29 +0300

    Added store name and description and location with there inputs

--- src/Pages/CreateStore.jsx
+++ src/Pages/CreateStore.jsx
@@ -1,14 +1,27 @@
-// src/Pages/CreateStorePage.js
 import React from "react";
 import "../styles/CreateStore.css";
 
 const CreateStorePage = () => {
   return (
     <div className="store-container">
-      <div className="text-center">
-        <h1>Create new store</h1>
+      <h1 className="title">Create New Store</h1>
+      <div className="form-container">
+        <div className="form-group">
+          <label htmlFor="store-name">Store</label>
+          <input id="store-name" type="text" placeholder="Store Name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="store-description">Description</label>
+          <textarea
+            id="store-description"
+            placeholder="Store Description"
+          ></textarea>
+        </div>
+        <div className="form-group">
+          <label htmlFor="store-location">Location</label>
+          <input id="store-location" type="text" placeholder="Store Location" />
+        </div>
       </div>
-      {/* Add other content here that does not need to be centered */}
     </div>
   );
 };
