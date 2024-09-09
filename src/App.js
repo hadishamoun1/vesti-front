ec0a9bc92a7a807fe7b3c393e84f8d5a13dbeae1
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-09 22:22:57 +0300

    Added navbar to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-// App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./Pages/Login";
 import SignupPage from "./Pages/Signup";
-
+import HomePage from "./Pages/Home"; 
+import Layout from "./Layout"; 
 function App() {
   return (
     <Router>
@@ -11,7 +11,15 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
-          {/* You can add more routes here */}
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <HomePage /> 
+              </Layout>
+            }
+          />
+          
         </Routes>
       </div>
     </Router>
