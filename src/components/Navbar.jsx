1c54084bc009469aaec506ef903461146671cf32
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-09 22:50:09 +0300

    Fixed navbar icon and text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Navbar.css'; 
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -12,26 +12,31 @@ const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {/* Logo Section */}
-        <div className="navbar-logo">
-          <Link to="/">
-            <img src="path/to/logo.png" alt="Logo" className="logo-image" /> {/* Replace with your logo path */}
-          </Link>
-        </div>
-
         {/* Navigation Links */}
-        <ul className={`navbar-menu ${menuOpen ? 'active' : ''}`}>
+        <ul className={`navbar-menu ${menuOpen ? "active" : ""}`}>
           <li className="navbar-item">
-            <Link to="/discounts" className="navbar-link">Discounts</Link>
+            <Link to="/discounts" className="navbar-link">
+              Discounts
+            </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/store" className="navbar-link">Store</Link>
+            <Link to="/" className="navbar-link">
+              <img src="path/to/logo.png" alt="Logo" className="logo-image" />{" "}
+              {/* Replace with your logo path */}
+            </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/products" className="navbar-link">Products</Link>
+            <Link to="/store" className="navbar-link">
+              Store
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/products" className="navbar-link">
+              Products
+            </Link>
           </li>
         </ul>
-        
+
         {/* Hamburger Menu Icon */}
         <div className="navbar-hamburger">
           <button className="hamburger-icon" onClick={toggleMenu}>
