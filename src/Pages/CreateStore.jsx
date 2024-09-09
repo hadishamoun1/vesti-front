3522f12a2c15cc4a0d0476f98129cef7939e48a6
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-09 22:57:22 +0300

    Created initial code for create store page

---
+++ src/Pages/CreateStore.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './StorePage.css'; // Import the CSS file
+
+const StorePage = () => {
+  return (
+    <div className="store-container">
+      <h1>Store Page</h1>
+      {/* Add content for the store page here */}
+    </div>
+  );
+};
+
+export default StorePage;
