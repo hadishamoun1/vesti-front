953edb608be333db79051e710019c4b2c8e84a27
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-09 19:17:09 +0300

    Added route to login page in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import LoginPage from './Pages/Login';  
 
 function App() {
-
+  return (
+    <div className="App">
+     
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
